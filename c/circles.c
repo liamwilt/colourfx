f6ee39949b4b127d6d414afc0092f9117d81e513
liamwilt <liam1wilt@gmail.com>
2016-04-29 19:47:38 -0400

    Edits to path and size

--- c/circles.c
+++ c/circles.c
@@ -12,9 +12,6 @@
 # define M_PI 3.141592653
 #endif
 
-// Compile with
-// clang -Wall -Wextra -std=c11 -g circle.c -o circle -lm -lGL -lglfw
-
 static void error(int error, const char *desc)
 {
         fputs(desc, stderr);
@@ -22,7 +19,6 @@ static void error(int error, const char *desc)
 
 static void key_callback(GLFWwindow *w, int key, int scancode, int action, int mods)
 {
-        // See http://www.glfw.org/docs/latest/group__keys.html
         if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS)
                 glfwSetWindowShouldClose(w, GL_TRUE);
 }
@@ -35,7 +31,7 @@ int main(int argc, char **argv)
         if (!glfwInit())
                 return 1;
 
-        w = glfwCreateWindow(500, 500, "Cirlces", NULL, NULL);
+        w = glfwCreateWindow(1024, 768, "Cirlces", NULL, NULL);
         if (!w)
         {
                 glfwTerminate();
@@ -51,7 +47,6 @@ int main(int argc, char **argv)
                 int width, height;
                 glfwGetFramebufferSize(w, &width, &height);
 
-                // Actual OpenGL calls
                 glClear(GL_COLOR_BUFFER_BIT);
                 glDisable(GL_DEPTH_TEST);
                 glMatrixMode(GL_PROJECTION);
@@ -61,38 +56,35 @@ int main(int argc, char **argv)
 
                 // Draw our line
                 glBegin(GL_LINE_LOOP);
-//                        int x1 = 100, y1 = 100;
-//                        int x2 = 200, y2 = 200;
-//
-                        glColor3f(0, 0, 1);
 
-                        // Framerate-based angel. As the frames are displayed, this integer will increment
+                        glColor3f(1, 0, 1);
+
+                        // Framerate-based angle. As the frames are displayed, this integer will increment
                         // and continue until it hits 360, then it will wrap around and start from 0
                         static double iteration = 0;
                         // The x, y offset onto the screen -- this should later be centered
-                        static const int offset = 150;
+                        static const int xoffset = 512;
+                        static const int yoffset = 384;
                         // The radius of both our circle and the circle it's spinning in.
-                        static const float radius = 50;
+                        static const float radius = 75;
 
-                        // Calculate our x, y cooredinates
-                        double x1 = offset + radius + 100 * cos(iteration);
-                        double y1 = offset + radius + 100 * sin(iteration);
+                        // Calculate our x, y coordinates
+                        double x1 = xoffset + radius * cos(iteration);
+                        double y1 = yoffset + radius * sin(iteration);
                         static double wobble = 0.0;
-                        //double x2 = offset + 100 + radius * cos(iteration), y2 = offset + 100 * sin(iteration);
-                        //
+                        
                         y1 += sin(wobble) * 100;
                         wobble += 0.01;
 
-                        // A = (π * r²)
                         double a = M_PI * (100 * 2);
-                        // C = (2 * π * r)
+                        
                         double c = 2 * M_PI * 100;
 
                         static double b = 128;
-                        //b += 1.0;
+                        
                         for (double i = 0; i < 2 * M_PI; i = i + ((2 * M_PI) / b))
                         {
-//                                glVertex2f(x1 + sin(i) * M_PI, y1 + cos(i) * M_PI);
+                                //glVertex2f(x1 + sin(i) * M_PI, y1 + cos(i) * M_PI);
 #if 0
                                 //   var = x pos + radius * cos(angle)
                                 double x = x1 + radius * cos(i);
@@ -101,20 +93,17 @@ int main(int argc, char **argv)
                                 glVertex2f(x, y);
 #endif
                                 glVertex2f(x1 + radius * cos(i), y1 + radius * sin(i));
-//                                glVertex2f(x1, y1);
-//                                glVertex2f(x2, y2);
+
                         }
 
-                        iteration += 0.01;
+                        iteration += 0.05;
 
                 glEnd();
 
 
-                // Swap buffers and look for events
                 glfwSwapBuffers(w);
                 glfwPollEvents();
-               // usleep(700000);
-                //sleep(1);
+
         }
 
         glfwDestroyWindow(w);
